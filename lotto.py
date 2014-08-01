7e2dafa6c2f4d4f4946097523d19e7704d400ede
Brian Ward <brian.g.ward@gmail.com>
2014-08-01 16:32:11 +0100

    added lotto

--- lotto.py
+++ lotto.py
@@ -10,35 +10,26 @@ from datetime import datetime
 from datetime import timedelta
 from datetime import time
 
-default_day = datetime.today().date()
 
-def lotto_day(chosen_day = default_day):
-
-    print('chosen_day :',chosen_day)
-    # print('time :', check_time)
-    day_date = datetime.today().date()
-    print(day_date)
+def lotto_day(chosen_day=datetime.today().date()):
 
     if isinstance(chosen_day, str):
-        chosen_day = datetime.strptime(chosen_day, '%Y-%m-%d')
-        print('chosen_day :',chosen_day)
+        chosen_day = datetime.strptime(chosen_day, '%Y-%m-%d').date()
     day = chosen_day.weekday()
-    print('todayday', day)
-    #the key is the day, the value is the number of days to next draw 0 = mon, 1 - tues etc
+    # the key is the day, the value is the number of days
+    # to next draw 0 = mon, 1 - tues etc
     lotto_day = {0: 2, 1: 1, 2: 0, 3: 2, 4: 1, 5: 0, 6: 3}
-    print ('diff', lotto_day[day])
 
     if lotto_day[day] == 0:
-        #if it is a lotto day then check the time
+        #  it is a lotto day then check the time
         check_time = datetime.today().time()
         if check_time < time(20, 0, 0):
-            next_date = day_date + timedelta(days=lotto_day[day] + 1)
-            print('next day', next_date)
+            next_date = chosen_day + timedelta(days=lotto_day[day] + 1)
     else:
-        next_date = day_date + timedelta(days=lotto_day[day])
+        next_date = chosen_day + timedelta(days=lotto_day[day])
 
-    print('The next lotto is on the ', next_date)
+    return next_date
 
 if __name__ == '__main__':
-    #lotto_day()
+    lotto_day()
     lotto_day('2014-07-18')
--- lotto_test.py
+++ lotto_test.py
@@ -1,5 +1,6 @@
 import unittest
 from lotto import lotto_day
+import datetime
 
 
 class annagramTestCase(unittest.TestCase):
@@ -7,21 +8,16 @@ class annagramTestCase(unittest.TestCase):
     def setUp(self):
         pass
 
-
     def tearDown(self):
         pass
 
-
     def test_sql_connector1(self):
-        pass
-        self.dedsert.add_user('brian1', 134, 'astronaut1')
-        self.assertEqual("[(1, 'brian1', 134, 'astronaut1')]",
-                         self.dedsert.__str__())
-        self.dedsert.add_user('brian2', 1345, 'astronaut2')
-        self.assertEqual("[(1, 'brian1', 134, 'astronaut1'), (2, 'brian2', 1345, 'astronaut2')]",
-                         self.dedsert.__str__())
-
-
+        self.assertEqual(datetime.date(2014, 8, 2),
+                         lotto_day())
+        self.assertEqual(datetime.date(2014, 7, 19),
+                         lotto_day('2014-07-18'))
+        self.assertEqual(datetime.date(2015, 1, 1),
+                         lotto_day('2014-12-31'))
 
 if __name__ == "__main__":
     unittest.main()
