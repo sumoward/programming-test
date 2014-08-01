392ddf5456124e4f3dcdbbf4be066211f2993acc
Brian Ward <brian.g.ward@gmail.com>
2014-08-01 17:40:15 +0100

    tidy up TODO: read over and code review

--- cache_decorator.py
+++ cache_decorator.py
@@ -15,45 +15,30 @@ def fib1(n):
     if n < 2:
         return n
     return fib1(n - 1) + fib1(n - 2)
+# i have also included my own implementation of a memoization decorator.
 
 
 def lru(maxsize=100):
-    #holder1 = {}
-
     # if there are decorator arguments, the function
-    # to be decorated Y       it a single argument uou can give to it
     def helper1(passed_function):
         holder1 = {}
-        buffer = collections.deque() 
+        buffer = collections.deque()
+
+        # internal function that we pass the params to
         def caching(param):
-            #holder1 = {}
             if param not in holder1:
                 holder1[param] = passed_function(param)
                 buffer.appendleft(param)
                 if len(buffer) > maxsize:
-                    #print('in cache')
-                     # find the entry
+                    # find the entry
                     old_key = buffer[maxsize]
-                    print(old_key)
-                    print('ok', old_key,holder1[old_key], len(holder1))
                     del holder1[old_key]
                     buffer.pop()
-                    #print(holder1)
             else:
-                # what do we do is the key is in our cache
-                pass
                 buffer.remove(param)
                 buffer.appendleft(param)
-                #print('in else', param, holder1)
-                
-                # what do we do if the key is not in our cache
-                #del  holder1[param]
-                #print('test', ans)
-            print(param, holder1)
             return holder1[param]
-        
         return caching
-    
     return helper1
 
 
@@ -68,18 +53,13 @@ if __name__ == '__main__':
     print(fib1.cache_info())
     [fib1(n) for n in range(16)]
     print(fib1.cache_info())
-    print ('*'*60)
+    print('*'*60)
     print(fib2(12))
-    print ('*'*60)
+    print('*'*60)
     print(fib2(9))
-    print ('*'*60)
+    print('*'*60)
     print(fib2(10))
 #     print ('*'*60)
 #     print(fib1(5))
-    print ('-'*60)
+    print('-'*60)
     print(fib2(5))
-    #print(fib2.cache_info())
-    #[fib2(n) for n in range(16)]
-    #print(fib2.cache_info())
-
-
